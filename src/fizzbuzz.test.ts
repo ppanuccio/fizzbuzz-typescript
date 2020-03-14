2644db731042953b690afb048f9a1d67a19acb62
Pasquale Panuccio <pasquale.panuccio@xpeppers.com>
2020-03-14 23:45:46 +0100

    First version

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -1,4 +1,4 @@
-import { FizzBuzz } from './fizzbuzz';
+import { FizzBuzzApp } from './fizzbuzz';
 
 /**
  * FizzBuzz test
@@ -9,14 +9,32 @@ describe('FizzBuzz', () => {
   });
 
   it('FizzBuzz is instantiable', () => {
-    expect(new FizzBuzz()).toBeInstanceOf(FizzBuzz);
+    expect(new FizzBuzzApp()).toBeInstanceOf(FizzBuzzApp);
   });
 
   describe('print', () => {
     it('should return 1 with 1', () => {
-      const fizzbuzz = new FizzBuzz();
+      const fizzbuzz = new FizzBuzzApp();
       const output = fizzbuzz.print(1);
       expect(output).toEqual("1");
+    });
+
+    it('should return Fizz with multiple of 3', () => {
+      const fizzbuzz = new FizzBuzzApp();
+      const output = fizzbuzz.print(3);
+      expect(output).toEqual("Fizz");
+    });
+
+    it('should return Buzz with multiple of 5', () => {
+      const fizzbuzz = new FizzBuzzApp();
+      const output = fizzbuzz.print(5);
+      expect(output).toEqual("Buzz");
+    });
+
+    it('should return FizzBuzz with multiple of 3 and 5', () => {
+      const fizzbuzz = new FizzBuzzApp();
+      const output = fizzbuzz.print(15);
+      expect(output).toEqual("FizzBuzz");
     });
   });
 });
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,6 +1,28 @@
-export class FizzBuzz {
+export class FizzBuzzApp {
 
     print(num: number): string {
+      if(this.isFizz(num)){
+        if(this.isBuzz(num)){
+          return "FizzBuzz";
+        }else{
+          return "Fizz";
+        }
+      }
+      if(this.isBuzz(num)){
+        if(this.isFizz(num)){
+          return "FizzBuzz"
+        }else{
+          return "Buzz";
+        }
+      }
       return num.toString();
     }
+
+  private isBuzz(num: number) {
+    return num % 5 === 0;
+  }
+
+  private isFizz(num: number) {
+    return num % 3 === 0;
+  }
   }
