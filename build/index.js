4c9dc9d4cb045c49bc6407d4c4c0ebe167327fc7
Pasquale Panuccio <pasquale.panuccio@xpeppers.com>
2020-03-19 23:54:51 +0100

    Rmoved build direcotry

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fizzbuzz_1 = require("./fizzbuzz");
-const fizzBuzz = new fizzbuzz_1.FizzBuzzApp();
-fizzBuzz.print();
-//# sourceMappingURL=index.js.map
